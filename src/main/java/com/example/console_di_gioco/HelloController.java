d5f5e83903af93f9f07c8990d94d60c88df53a88
alex.fiorini_isarome <61363883+AlexFiorini@users.noreply.github.com>
2023-03-07 14:16:39 +0100

    Primo commit

---
+++ src/main/java/com/example/console_di_gioco/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.console_di_gioco;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
