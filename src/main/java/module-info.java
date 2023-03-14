55ad2d47323de9f6a85a6fe31a541bfa904a2341
alex.fiorini_isarome <61363883+AlexFiorini@users.noreply.github.com>
2023-03-14 14:46:44 +0100

    Cambiate directory

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,16 @@
-module com.tris {
+module com.example.console_di_gioco {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
 
-    exports com.tris;
-    opens com.tris to javafx.fxml;
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com to javafx.fxml;
+    exports com;
 }
