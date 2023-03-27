1152fc7347476d1231857b74639c32afd9e4a86f
Gabriele Rosso <gabrielerosso784@gmail.com>
2023-03-27 10:49:26 +0200

    Immagini e suoni aggiunti

--- src/main/java/com/numeri/Main.java
+++ src/main/java/com/numeri/Main.java
@@ -1,4 +1,4 @@
-package com.console_di_gioco;
+package com.numeri;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,17 +7,18 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("2048.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module com.example.console_di_gioco {
+module com.numeri {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
@@ -11,6 +11,6 @@ module com.example.console_di_gioco {
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
-    opens com.console_di_gioco to javafx.fxml;
-    exports com.console_di_gioco;
+    opens com.numeri to javafx.fxml;
+    exports com.numeri;
 }
