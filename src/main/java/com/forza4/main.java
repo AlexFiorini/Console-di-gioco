bb5d62d72574292a9f811e3b9f4b7553aaf54c1f
alex.fiorini_isarome <61363883+AlexFiorini@users.noreply.github.com>
2023-04-17 10:44:05 +0200

    Aggiunta interfaccia

--- src/main/java/com/forza4/main.java
+++ src/main/java/com/forza4/main.java
@@ -1,24 +1,22 @@
 package com.forza4;
 
-public class main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Finestra.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Forza4");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-
-        griglia= new campo
-        p1= new player("")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        launch();
     }
 }
