8292fa4f39940a77d8c4949a9158e3bbcca63eed
AlexFiorini <61363883+AlexFiorini@users.noreply.github.com>
2023-04-20 18:14:07 +0200

    Aggiunte immagini e css

--- src/main/java/com/console_di_gioco/Main.java
+++ src/main/java/com/console_di_gioco/Main.java
@@ -4,19 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 480, 480);
-        scene.getStylesheets().addAll(this.getClass().getResource("Test.css").toExternalForm());
+        scene.getStylesheets().addAll(Objects.requireNonNull(this.getClass().getResource("Test.css")).toExternalForm());
+        stage.setResizable(false);
         stage.setTitle("Menu");
         stage.setScene(scene);
         stage.show();
-        //
     }
 
     public static void main(String[] args) {
