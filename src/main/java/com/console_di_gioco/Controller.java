033ceb723966ebcf35dd86a83eed0a23d6870e7c
GabrieleRosso <gabrielerosso784@gmail.com>
2023-04-20 17:12:35 +0200

    Immagini giochi

---
+++ src/main/java/com/console_di_gioco/Controller.java
@@ -0,0 +1,14 @@
+package com.console_di_gioco;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    public Button tris = new Button();
+    public Button forza4 = new Button();
+    public Button bn = new Button();
+    public Button g2048 = new Button();
+
+}
