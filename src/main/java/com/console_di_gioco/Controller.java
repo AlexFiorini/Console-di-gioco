b0c7b38d3929bbea04ca1be5ec1925c94cf833f0
GabrieleRosso <gabrielerosso784@gmail.com>
2023-04-20 17:15:27 +0200

    Immagini giochi

--- src/main/java/com/console_di_gioco/Controller.java
+++ src/main/java/com/console_di_gioco/Controller.java
@@ -3,12 +3,15 @@ package com.console_di_gioco;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import java.util.Objects;
+import java.awt.*;
 
 public class Controller {
 
-    public Button tris = new Button();
-    public Button forza4 = new Button();
-    public Button bn = new Button();
-    public Button g2048 = new Button();
+    Image tris = new Image(Objects.requireNonNull(getClass().getResourceAsStream("tris.png")));
+    public Button bTris = new Button();
+    public Button bForza4 = new Button();
+    public Button bBN = new Button();
+    public Button b2048 = new Button();
 
 }
