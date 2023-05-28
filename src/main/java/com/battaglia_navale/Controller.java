b1a3a0db861a4638b4746258a4487f380a757bde
AlexFiorini <61363883+AlexFiorini@users.noreply.github.com>
2023-05-28 14:37:39 +0200

    Aggiunto test e bottoni

--- src/main/java/com/battaglia_navale/Controller.java
+++ src/main/java/com/battaglia_navale/Controller.java
@@ -1,6 +1,23 @@
 package com.battaglia_navale;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+    //Get all columns and rows from Battaglia-navale.fxml
+    @FXML
+    GridPane tabella;
+    Button[][] buttons = new Button[10][10];
 
-
+    public void test() {
+        System.out.println(tabella);
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++){
+                buttons[i][j] = new Button();
+                buttons[i][j].setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+                tabella.add(buttons[i][j], i, j);
+            }
+        }
+    }
 }
