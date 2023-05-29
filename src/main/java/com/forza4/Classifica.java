5ad2e7aac471d789f545ae2ab8631a08d2ecce64
AlexFiorini <61363883+AlexFiorini@users.noreply.github.com>
2023-05-29 22:15:03 +0200

    Aggiunta classifica caricata da file

--- src/main/java/com/forza4/Classifica.java
+++ src/main/java/com/forza4/Classifica.java
@@ -1,20 +1,46 @@
 package com.forza4;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-public class Classifica {
+public class Classifica implements Initializable {
 
     @FXML
-    Label lRosso = new Label();
+    Label lRosso;
     @FXML
-    Label lBlu = new Label();
-    @FXML
-    Label l1 = new Label();
-    @FXML
-    Label l2 = new Label();
+    Label lBlu;
 
-    public void setLabels(int cont, int cont1){
-        this.lRosso.setText(Integer.toString(cont));
-        this.lBlu.setText(Integer.toString(cont1));
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        int contRosso = 0;
+        int contBlu = 0;
+        try {
+                FileReader fr = new FileReader("src/main/java/com/forza4/save.txt");
+                BufferedReader br = new BufferedReader(fr);
+                String line;
+                if ((line = br.readLine()) != null) {
+                    contRosso = Integer.parseInt(line);
+                } else {
+                    contRosso = 0;
+                }
+                if ((line = br.readLine()) != null) {
+                    contBlu = Integer.parseInt(line);
+                } else {
+                    contBlu = 0;
+                }
+                br.close();
+                fr.close();
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        lRosso.setText(Integer.toString(contRosso));
+        lBlu.setText(Integer.toString(contBlu));
     }
 }
