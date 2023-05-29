feb116aa1e987c3683b2379f69549dc8309127d3
alex.fiorini_isarome <61363883+AlexFiorini@users.noreply.github.com>
2023-05-29 10:48:05 +0200

    Aggiunto controllo bottoni

--- src/main/java/com/battaglia_navale/Controller.java
+++ src/main/java/com/battaglia_navale/Controller.java
@@ -3,6 +3,7 @@ package com.battaglia_navale;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.event.ActionEvent;
 
 public class Controller {
     //Get all columns and rows from Battaglia-navale.fxml
@@ -16,11 +17,13 @@ public class Controller {
             for(int j = 0; j < 10; j++){
                 buttons[i][j] = new Button();
                 buttons[i][j].setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+                buttons[i][j].setOnAction(e -> onClick(e));
                 tabella.add(buttons[i][j], i, j);
             }
         }
     }
 
-    // Make battleship game
-
+    public void onClick(ActionEvent e) {
+        System.out.println("Row: " + GridPane.getRowIndex((Button)e.getSource()) + " Column: " + GridPane.getColumnIndex((Button)e.getSource()));
+    }
 }
